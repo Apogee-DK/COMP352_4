4fe385beeba71bfc40c09f0a761864c639d05eec
Dexter K <Apogee-DK@users.noreply.github.com>
2015-11-28 14:59:36 -0500

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -2,69 +2,113 @@
 public class HashMap {
 
 	private int capacity;
-	
 	private HashEntry[] table;
+		
 	
-	public HashMap(int cap) {
-		
+	public HashMap(int cap){
 		table = new HashEntry[cap];
-		
 		for (int i=0; i < cap; i++)
 			table[i] = null;
-		
 	}
 	
+	
+	
+	
+	//GETTING THE VALUE FROM A SPECIFIC KEY	
 	public String get(String k) {
-		
-		//hash function to decode
-		
-		
-		//sample - linear probing
-		
-		//need to compute hash for given k
-		// use java's hashcode for now
-		int kValue = k.hashCode();
-		
-        int hash = (kValue % capacity);
-        
-        while (table[hash] != null && table[hash].getKey() != k)
-              hash = (hash + 1) % capacity;
-        if (table[hash] == null)
-              return " ";
+		int hashVal = hashMe(k, capacity);
+        if (table[hashVal] == null){
+              return null;
+        }
         else
-              return table[hash].getValue();
-        
-        
+              return table[hashVal].getValue();
 	}
 	
 	
 	
-	public void put (String k, String v) {
-		
-		
-		//hash function  to encode
-		
-		//sample - linear probing
-		
-		//need to compute hash for given k
-		// use java's hashcode for now
-		int kValue = k.hashCode();
-		
-        int hash = (kValue % capacity);
-        while (table[hash] != null && table[hash].getKey() != k)
-              hash = (hash + 1) % capacity;
-        table[hash] = new HashEntry(k, v);
 	
-        
-		
+	//ADDING VALUES INTO THE TABLE
+	public void put(String k, String v) {
+		int hashVal = hashMe(k, capacity);
+        table[hashVal] = new HashEntry(k, v);
+	
 	}
 	
 	
+	
+	
+	//REMOVE A CERTAIN STRING BY USING THE KEY
 	public void remove(String k){
+		System.out.println("Trying to remove " + k + ". Searching for it in the Hash Table...");
+		if (get(k) == null){
+			System.out.println(k + "was not found in the Table.");
+		}
+		else{
+			int hashVal = hashMe(k, capacity);
+			table[hashVal] = new DeletedEntry(); //LEAVING A TRACE, SO WE KNOW THERE WAS AN ELEMENT THERE		
+		}
+			
 		
-		get(k);
+	}
+	
+	//DETERMINE THE SIZE OF THE HASH TABLE
+	public int size(){
+		return table.length;		
+	}
+	
+	
+	
+	//DETERMINE IF THE TABLE IS EMPTY
+	public boolean isEmpty(){
+		for(HashEntry h: table){
+			if(h != null)
+				return false;
+		}
+		return true;
+	}
+	
+	
+	//HASHING FUNCTION - I ADDED THIS TO THE HASHMAP CLASS
+	public int hashMe(String k, int cap) {
+		//k is the key, N is the capacity of array/ hash table
+		//############# HASH CODE MAP ##############
+		// converts strings to integers
 		
+		int len = k.length();
+		int z = 33;		//good prime number to avoid collisions
+		int total = 0;
 		
+		for (int i=0; i < len; i++) {
+			int val = (int) k.charAt(i);
+			val = val *  ((int)  (Math.pow(z, i)) );
+			total += val;
+		}
+		
+		//now total is the integer equivalent of the string
+		System.out.println("Integer equiv of " + k + " is " + total);
+		
+		//######## COMPRESSION MAP
+		// integers to array index
+		// we use MAD 
+		int a = cap-1;	// a mod N != 0
+		int b = 2;		// b can be any nonnegative int
+		
+		int finalKey = ((a * total) + b ) % cap;
+		
+		while(isEmptyCell(finalKey) == false){
+			//LENMOR, CHECK THIS IF YOU ARE REWORKING THE HASHME FUNCTION, I BASICALLY MOD THE WHOLE THING AGAIN IF THE CELL IS NOT EMPTY
+			finalKey = ((a * finalKey) + b) % cap;  
+		}	
+		
+		return finalKey;
+		
+	}
+	
+	//CHECKS IF THE CELL IS EMPTY
+	public boolean isEmptyCell(int hashVal){
+		if(table[hashVal] == null || table[hashVal].equals("-1")) //WHEN I REMOVE A VALUE FROM THE TABLE, I INPUT -1 TO LEAVE A TRACE
+			return true;		
+		return false;
 	}
 	
 }
