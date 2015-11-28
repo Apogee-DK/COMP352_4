4934bf621a55e87fbbd0e0c09b2666d7fb5c63c8
Dexter K <Apogee-DK@users.noreply.github.com>
2015-11-28 15:01:24 -0500

    Update DeletedEntry.java

--- DeletedEntry.java
+++ DeletedEntry.java
@@ -1,6 +1,6 @@
 //This will allow us to know if the hash cell had an entry or not
 public class DeletedEntry extends HashEntry{
-	private static DeletedEntry delEntry = null;
+	//private static DeletedEntry delEntry = null;
 	
 		
 	//For entries that are removed!
