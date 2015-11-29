b37b9cb9ffc3cb05fe962e45d35a31607586a121
Dexter K <Apogee-DK@users.noreply.github.com>
2015-11-28 20:31:58 -0500

    Update Available.java

--- Available.java
+++ Available.java
@@ -1,16 +1,6 @@
 //Object to see if the cell is available or not (if there was an element that was removed)
-
 public class Available {
-	boolean isAvailable;
 	
-	public Available(){
-		isAvailable = true;
-	}
-	
-	public boolean getAvailability(){
-		return isAvailable;
-	}
-	
-	
+	public Available(){}
 	
 }
