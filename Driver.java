f1a2a63126d66a81108291ef4adfa38fe079a618
lenmorld <lenmorld@gmail.com>
2015-11-29 13:27:21 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -6,6 +6,7 @@ public class Driver {
 	
 	public static void main(String [] args){
 		
+
 		/*
 		HashMap h1 = new HashMap(100);
 		h1.setEmptyMarkerScheme('N');
@@ -63,16 +64,30 @@ public class Driver {
 		}
 		*/
 		
+		// 1st text file
+		//1st case , N, Q, resize = 1.3, loadFactor = 0.75
+		deliverableC('N','Q',"1.3",0.75, "hash_test_file1.txt" );
 		
-		HashMap h2 = new HashMap(100);
-		readFile(h2, "hash_test_file1.txt");
-		
-		HashMap h3 = new HashMap(100);
-		readFile(h3, "hash_test_file2.txt");
-		
+		// 1st text file
+		//1st case , N, D, resize = 1.2, loadFactor = 0.5
+		deliverableC('N','D',"1.2",0.5, "hash_test_file2.txt" );
+
 	}
 
-	public static void readFile (HashMap h, String filename)  {
+	/**
+	 * 
+	 * @param h HashMap
+	 * @param filename filename to load
+	 * @param bound first N elements in the file to load, -1 for all
+	 */
+	public static void readFile (HashMap h, String filename, int bound)  {
+		
+		boolean bounded = true;
+		int ctr=0;
+		
+		if (bound == -1)
+			bounded = false;
+			
 		
 		h.setEmptyMarkerScheme('N');
 		h.setCollisionHandling('D');
@@ -87,14 +102,113 @@ public class Driver {
 			e.printStackTrace();
 		}
 		
-		while (inFile.hasNextLine()) {
-				String line = inFile.nextLine();	
+		while (inFile.hasNextLine() ) {
+			
+			if (bounded &&  ctr >= bound  )		//if bound given, break upon hitting
+				break;
+			
+				String line = inFile.nextLine();		
 				h.put(line, line);
-				System.out.println(line);
+				//System.out.println(line);			//##### UNCOMMENT IT TO SEE Strings ###
+				
+				ctr++;
 		}
 
 		inFile.close();
 		
+	}
+	
+	public static void deliverableC(char e, char c, String rf, double rt, String filename)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		//##################################
+		//deliverable (c. i)
+
+		HashMap h = new HashMap(100);
+		
+		h.setEmptyMarkerScheme(e);
+		h.setCollisionHandling(c);
+		h.setRehashFactor(rf);
+		h.setRehashThreshold(rt);
+	
+		readFile(h, filename, 1000);		//first n elements
+		System.out.println("First 1000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 3000);	
+		System.out.println("First 3000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 5000);	
+		System.out.println("First 5000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 10000);	
+		System.out.println("First 10000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 50000);	
+		System.out.println("First 50000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 100000);
+		System.out.println("First 100000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 150000);
+		System.out.println("First 150000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, 200000);
+		System.out.println("First 200000 of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		readFile(h, filename, -1);		// ALL
+		System.out.println("ALL of file " + filename);
+		h.printHastableStatistic();
+		h.resetHashtableStatistics();
+		
+		System.out.println("Press ENTER to continue to next test...");
+		kb.nextLine();
+		
+		//##### TO DO #########
+		// c ii - remove
+		
+		
+
 	}
 	
 }
