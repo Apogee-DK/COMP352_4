32c5853715287b2b9ea09c8a53c5543f68c96905
Dexter K <Apogee-DK@users.noreply.github.com>
2015-11-30 02:05:50 -0500

    Update Available.java

--- Available.java
+++ Available.java
@@ -1,4 +1,9 @@
-//Object to see if the cell is available or not (if there was an element that was removed)
+/*
+ * Class Available
+ * 
+ * The following class is served as a marker which will serve to tell us whether a cell held an element or not.
+ * It will prevent any unwanted stop during searches in the hash table.
+ */
 public class Available {
 	
 	public Available(){}
